7ac0ec12011bd154d2e7ac7a845532e91edb85e8
c4ffein <c4ffein@gmail.com>
2026-03-04 17:22:44 +0100

    docs: add a comment for why we don't implement more CSRF mitigations for this demo app

--- apps/api/nitro.config.ts
+++ apps/api/nitro.config.ts
@@ -4,6 +4,7 @@ export default defineNitroConfig({
   srcDir: "server",
   errorHandler: "~/error-handler",
   routeRules: {
+    // Wide-open CORS is fine for now: auth uses JWT in Authorization header, no cookies, CSRF risk low-enough for now
     '/api/**': { cors: true, headers: { 'access-control-allow-methods': '*' } },
   }
 });
